f5a075a92c3fdc9ce46f265ff30366e3a7801294
garyghayrat <gary.ghayrat@pm.me>
2023-07-03 20:49:32 -0400

    Change provider order

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,8 +11,8 @@ import { publicProvider } from "wagmi/providers/public";
 const { chains, publicClient } = configureChains(
   [sepolia, foundry],
   [
-    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID as string }),
     publicProvider(),
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID as string }),
   ]
 );
 
